f9d29778929bc2ccedceb9956da537f4eacfd541
TheDogsNose <94315145+TheDogsNose@users.noreply.github.com>
2023-04-15 18:30:56 +0200

    deleted cs

--- Web/JS/scrip.js
+++ Web/JS/scrip.js
@@ -25,6 +25,18 @@ const c = 1;
     Symbols are unique variables so comparing two symbols will always result in false.
     eache data type has it's own constructor (called factory function)
 */
+/*  operations;
+        ++, --., -, +                           as in c both post and pre
+        +, -, *, /, %, **                       regular operations (** is power and % is remainder)
+        =, +=, -=, *=, /=, %=, **=              assignment
+        ==, !=, ===, !==, <, >, <=, >=          comparing operations (== checks if they are equal while === checks if they are equal and are of the same type/point to the same object)
+        !, &&, ||                               boolean operations
+        typeof (returns the type), instanceof (returns weither an object is an instance of an object type)
+        &, |, ~, ^, <<, >>, >>>                 bitwise operations (>>> is for unsigned right shift)
+        (condition)?(do this):(else do this)    teranery operations
+*/
+
+
 
 //function
 function func(a, b){
